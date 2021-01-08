fc22d1e4d424aff674ead7c8748e37c453a11494
Kevin DeMarco <kevin.demarco@gmail.com>
2021-01-08 18:24:47 -0500

    testing bringup on hardware

--- bamboomba_bringup/launch/bamboomba.launch.py
+++ bamboomba_bringup/launch/bamboomba.launch.py
@@ -1,24 +1,19 @@
+from os import path
 from launch import LaunchDescription
 from ament_index_python.packages import get_package_share_directory
 from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import FrontendLaunchDescriptionSource, PythonLaunchDescriptionSource,
+from launch.launch_description_sources import FrontendLaunchDescriptionSource, PythonLaunchDescriptionSource
 from nav2_common.launch import RewrittenYaml
 
 def generate_launch_description():
-    create_bringup_dir = get_package_share_directory('create_bringup_dir')
+    bamboomba_bringup_dir = get_package_share_directory('bamboomba_bringup')
 
-    original_params_file = path.join(
-        create_bringup_dir, 'config', 'default.yaml')
-
-    param_substitutions = {'dev': '/dev/create_serial'}
-
-    configured_params = RewrittenYaml(
-        source_file=original_params_file,
-        param_rewrites=param_substitutions,
-        convert_types=True)
+    base_launch_file = path.join(bamboomba_bringup_dir, 'launch', 'base.launch.py')
+    rp_lidar_a2_launch_file = path.join(bamboomba_bringup_dir, 'launch', 'rplidar_a2.launch.py')
 
     return LaunchDescription([
         IncludeLaunchDescription(
-            FrontendLaunchDescriptionSource(ros2_cpp_params_example_launch_file),
-            launch_arguments={'config': configured_params}.items())
+            PythonLaunchDescriptionSource(base_launch_file)),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(rp_lidar_a2_launch_file))
     ])
